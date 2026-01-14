d076585e01917ed1f09d74d97e973ffb5361be29
Mateus <mateusvidal199907@gmail.com>
2026-01-14 17:55:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ except Exception as e:
     st.error(f"Erro de conexão: {e}")
     st.stop()
 
-st.title("🏥 Sistema de Agendamento & Triagem")
+st.title("🏥 Sistema de Agendamento e Triagem")
 
 aba_cadastro, aba_agenda = st.tabs(["📝 Novo Agendamento", "📅 Consultar e Atualizar Status"])
 
@@ -149,5 +149,6 @@ with aba_agenda:
             
     else:
         st.info("Ainda não há agendamentos cadastrados.")
+
 
 
